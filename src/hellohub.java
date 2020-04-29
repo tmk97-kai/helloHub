326b6632c3824ad61b397716b91a236524d55703
kai <kai@kai-magicbook>
2020-04-29 11:37:41 +0800

    hh

--- src/hellohub.java
+++ src/hellohub.java
@@ -3,7 +3,7 @@ public class hellohub {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Hub!");
+		System.out.println("Hello Hub!  wow you can really dance");
 	}
 
 }
